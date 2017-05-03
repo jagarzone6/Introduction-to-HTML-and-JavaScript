57be447e39971d80fb2d1f6bd4c357ce0a29b9b3
jgarzon <jgarzon@easysol.net>
2017-05-03 16:40:45 -0500

    mod 4 js

---
+++ Module-4/Examples/scripts/script-4.js
@@ -0,0 +1,13 @@
+var $elem = document.getElementById('output');
+$elem.innerHTML += 4 + 6 + ' Demo';
+$elem.innerHTML += '\r\n Demo ' + 4 + 6;
+$elem.innerHTML += '\r\n Demo ' + (4 + 6);
+var cars = ["Saab", "Volvo", "BMW"];
+cars[0] // Saab
+cars[1] // Volvo
+cars[2] // BMW
+
+var arrayA = [];
+myArray[0] = Date.now;
+myArray[1] = myFunc;
+myArray[2] = new Array("Demo", "Test");
