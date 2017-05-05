322485b2beea2713a5d41244cf533b70d2836626
jgarzon <jgarzon@easysol.net>
2017-05-05 15:49:59 -0500

    mod 5

--- Module-5/Examples/Angular/js/script-4-routing.js
+++ Module-5/Examples/Angular/js/script-4-routing.js
@@ -23,10 +23,10 @@ app.config(function($routeProvider) {
 });
 app.factory ('model', function () {
     var data = [
-        { id: 0, title: 'Degree', icon: 'img/degree.png', description: 'First, you might graduate from college.' },
-        { id: 1, title: 'Networking', icon: 'img/networking.png', description: 'Then you could network with colleagues.' },
-        { id: 2, title: 'Certification', icon: 'img/certification.png', description: 'Earn a certification to stay ahead of the pack' },
-        { id: 3, title: 'Job Hunt', icon: 'img/jobhunt.png', description: 'Take your network and certifications on the hunt for the coolest jobs.' }
+        { id: 0, title: 'Degree', icon: 'img/angular_routing_degree.png', description: 'First, you might graduate from college.' },
+        { id: 1, title: 'Networking', icon: 'img/angular_routing_networking.png', description: 'Then you could network with colleagues.' },
+        { id: 2, title: 'Certification', icon: 'img/angular_routing_certification.png', description: 'Earn a certification to stay ahead of the pack' },
+        { id: 3, title: 'Job Hunt', icon: 'img/angular_routing_jobhunt.png', description: 'Take your network and certifications on the hunt for the coolest jobs.' }
     ];
     return {
         getAll:function () {
